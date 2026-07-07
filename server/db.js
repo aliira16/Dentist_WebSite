77aab78782047365a73e1064c39dfcf8df937195
aliira16 <rouaneali27@gmail.com>
2026-07-07 21:53:59 +0100

    feat(DB): make the based DB structure and link it with server in db.js

---
+++ server/db.js
@@ -0,0 +1,14 @@
+import pg from "pg";
+const { Pool } = pg;
+
+dotenv.config();
+
+const pool = new Pool({
+  user: process.env.DBuser,
+  host: process.env.DBhost,
+  database: process.env.DBdatabase,
+  password: process.env.DBpassword,
+  port: process.env.DBport,
+});
+
+export default pool;
