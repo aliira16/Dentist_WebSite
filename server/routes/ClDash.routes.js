bc72575019e7f178c9271f900d4c4c9a88719ee7
aliira16 <rouaneali27@gmail.com>
2026-07-08 12:33:29 +0100

    codeRabbit test

--- server/routes/ClDash.routes.js
+++ server/routes/ClDash.routes.js
@@ -2,3 +2,8 @@ import e from "express";
 import pool from "../db.js";
 
 const router = e.Router();
+
+router.get("/login-ClDash", async (req, res) => {
+  const result = await pool.query("SELECT * FROM ClDB");
+  res.json(result.rows);
+});
